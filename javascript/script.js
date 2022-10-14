52fea476f7f8705060bb5e3664a82e1bf3967e32
G-Moreira <gaabrielmooreira@gmail.com>
2022-10-14 14:34:28 -0300

    projeto drivenEats com envio do pedido

--- javascript/script.js
+++ javascript/script.js
@@ -156,8 +156,38 @@ function habilitarPedido() {
     if (pratoEscolhido && bebidaEscolhida && sobremesaEscolhida) {
         pedido.classList.add('habilitado');
         botaoTexto.innerHTML = "Fechar Pedido";
+
+        // Mudar o Link do Fechar Pedido
+        const nomeDoPrato = document.querySelector('.prato .selecionado h3');
+        const nomeDaBebida = document.querySelector('.bebida .selecionado h3');
+        const nomeDaSobremesa = document.querySelector('.sobremesa .selecionado h3');
+
+        const valorPrato = document.querySelector('.prato .selecionado .item-valor span');
+        const valorBebida = document.querySelector('.bebida .selecionado .item-valor span');
+        const valorSobremesa = document.querySelector('.sobremesa .selecionado .item-valor span');
+        
+        const total = Number( (valorPrato.innerHTML).replace(',','.') ) + Number( (valorBebida.innerHTML).replace(',','.') ) + Number( (valorSobremesa.innerHTML).replace(',','.') );
+        pedido.href="https://wa.me/5521988815425?text=" + 
+        encodeURIComponent(
+            "Olá, gostaria de fazer o pedido:" + 
+            "\n- Prato: " + nomeDoPrato.innerHTML +
+            "\n- Bebida: " + nomeDaBebida.innerHTML +
+            "\n- Sobremesa: " + nomeDaSobremesa.innerHTML +
+            "\nTotal: R$ "  + total.toFixed(2)
+        );
+
     } else if (pedido.classList.contains('habilitado')){
         pedido.classList.remove('habilitado');
         botaoTexto.innerHTML = "Selecione os 3 itens para fechar o pedido";
+        pedido.href="#";
+    }
+}
+
+function fazerPedido() {
+    const pedido = document.querySelector('.fechar-pedido');
+    if(pedido.classList.contains('habilitado')){
+
+
+
     }
 }
