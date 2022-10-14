5d65d6e7cf6e68f0773f8d8e259ec932aa95af18
G-Moreira <gaabrielmooreira@gmail.com>
2022-10-14 10:34:07 -0300

    projeto drivenEats com selecao de itens

---
+++ javascript/script.js
@@ -0,0 +1,136 @@
+
+function pratoUm() {
+    const pratoSelec = document.querySelector('.prato .opcaoUm');
+    pratoSelec.classList.toggle('selecionado');
+
+    const retiraSelec = document.querySelector('.prato .opcaoDois');
+    const retiraSelec2 = document.querySelector('.prato .opcaoTres');
+
+    if(retiraSelec.classList.contains('selecionado')){
+        retiraSelec.classList.remove('selecionado');
+    }
+    if(retiraSelec2.classList.contains('selecionado')){
+        retiraSelec2.classList.remove('selecionado');
+    }
+}
+
+function pratoDois() {
+    const pratoSelec = document.querySelector('.prato .opcaoDois');
+    pratoSelec.classList.toggle('selecionado');
+
+    const retiraSelec = document.querySelector('.prato .opcaoUm');
+    const retiraSelec2 = document.querySelector('.prato .opcaoTres');
+
+    if(retiraSelec.classList.contains('selecionado')){
+        retiraSelec.classList.remove('selecionado');
+    }
+    if(retiraSelec2.classList.contains('selecionado')){
+        retiraSelec2.classList.remove('selecionado');
+    }
+}
+
+function pratoTres() {
+    const pratoSelec = document.querySelector('.prato .opcaoTres');
+    pratoSelec.classList.toggle('selecionado');
+
+    const retiraSelec = document.querySelector('.prato .opcaoUm');
+    const retiraSelec2 = document.querySelector('.prato .opcaoDois');
+
+    if(retiraSelec.classList.contains('selecionado')){
+        retiraSelec.classList.remove('selecionado');
+    }
+    if(retiraSelec2.classList.contains('selecionado')){
+        retiraSelec2.classList.remove('selecionado');
+    }
+}
+
+function bebidaUm() {
+    const bebidaSelec = document.querySelector('.bebida .opcaoUm');
+    bebidaSelec.classList.toggle('selecionado');
+
+    const retiraSelec = document.querySelector('.bebida .opcaoDois');
+    const retiraSelec2 = document.querySelector('.bebida .opcaoTres');
+
+    if(retiraSelec.classList.contains('selecionado')){
+        retiraSelec.classList.remove('selecionado');
+    }
+    if(retiraSelec2.classList.contains('selecionado')){
+        retiraSelec2.classList.remove('selecionado');
+    }
+}
+
+function bebidaDois() {
+    const bebidaSelec = document.querySelector('.bebida .opcaoDois');
+    bebidaSelec.classList.toggle('selecionado');
+
+    const retiraSelec = document.querySelector('.bebida .opcaoUm');
+    const retiraSelec2 = document.querySelector('.bebida .opcaoTres');
+
+    if(retiraSelec.classList.contains('selecionado')){
+        retiraSelec.classList.remove('selecionado');
+    }
+    if(retiraSelec2.classList.contains('selecionado')){
+        retiraSelec2.classList.remove('selecionado');
+    }
+}
+
+function bebidaTres() {
+    const bebidaSelec = document.querySelector('.bebida .opcaoTres');
+    bebidaSelec.classList.toggle('selecionado');
+
+    const retiraSelec = document.querySelector('.bebida .opcaoUm');
+    const retiraSelec2 = document.querySelector('.bebida .opcaoDois');
+
+    if(retiraSelec.classList.contains('selecionado')){
+        retiraSelec.classList.remove('selecionado');
+    }
+    if(retiraSelec2.classList.contains('selecionado')){
+        retiraSelec2.classList.remove('selecionado');
+    }
+}
+
+function sobremesaUm() {
+    const sobremesaSelec = document.querySelector('.sobremesa .opcaoUm');
+    sobremesaSelec.classList.toggle('selecionado');
+
+    const retiraSelec = document.querySelector('.sobremesa .opcaoDois');
+    const retiraSelec2 = document.querySelector('.sobremesa .opcaoTres');
+
+    if(retiraSelec.classList.contains('selecionado')){
+        retiraSelec.classList.remove('selecionado');
+    }
+    if(retiraSelec2.classList.contains('selecionado')){
+        retiraSelec2.classList.remove('selecionado');
+    }
+}
+
+function sobremesaDois() {
+    const sobremesaSelec = document.querySelector('.sobremesa .opcaoDois');
+    sobremesaSelec.classList.toggle('selecionado');
+
+    const retiraSelec = document.querySelector('.sobremesa .opcaoUm');
+    const retiraSelec2 = document.querySelector('.sobremesa .opcaoTres');
+
+    if(retiraSelec.classList.contains('selecionado')){
+        retiraSelec.classList.remove('selecionado');
+    }
+    if(retiraSelec2.classList.contains('selecionado')){
+        retiraSelec2.classList.remove('selecionado');
+    }
+}
+
+function sobremesaTres() {
+    const sobremesaSelec = document.querySelector('.sobremesa .opcaoTres');
+    sobremesaSelec.classList.toggle('selecionado');
+
+    const retiraSelec = document.querySelector('.sobremesa .opcaoUm');
+    const retiraSelec2 = document.querySelector('.sobremesa .opcaoDois');
+
+    if(retiraSelec.classList.contains('selecionado')){
+        retiraSelec.classList.remove('selecionado');
+    }
+    if(retiraSelec2.classList.contains('selecionado')){
+        retiraSelec2.classList.remove('selecionado');
+    }
+}
+
